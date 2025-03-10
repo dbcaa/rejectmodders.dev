506350c0f40a51e253cdb6dd82614f4a509c8954
RejectModders <142326052+RejectModders@users.noreply.github.com>
2025-03-10 07:43:17 -0500

    Update friends.ts

--- src/config/friends.ts
+++ src/config/friends.ts
@@ -2,27 +2,23 @@
 export const friendsHeadLine = "RejectModders Friends"
 export const friendsIntro = "Meet some interesting friends."
 
-
 // friends
 export type FriendItemType = {
   name: string
   description?: string
   link: { href: string, label?: string }
-  logo?: string
-  image?: string
+  image: string 
 }
-
 
 export const friends: Array<FriendItemType> = [
   {
     name: 'Corey Chiu',
     link: { href: 'https://coreychiu.com' },
-    image: 'corey-chiu.jpg', 
+    image: 'corey-chiu.jpg',
   },
   {
     name: 'RejectModders',
     link: { href: 'https://pornhub.com' },
-    image: 'rejectmodders.jpg',
+    image: 'rejectmodders.gif',
   },
 ]
-
