1e36b573140816b6710e30336e235a9ef99eab17
RejectModders <142326052+RejectModders@users.noreply.github.com>
2025-03-10 10:12:08 -0500

    Update projects.ts

--- src/config/projects.ts
+++ src/config/projects.ts
@@ -67,58 +67,27 @@ export type ProjectItemType = {
   
   export const githubProjects: Array<ProjectItemType> = [
     {
-      name: 'Devtoolset',
-      description: 'Open-source & database-free developer tools navigator / 开源无数据库配置的开发者工具导航站',
-      link: { href: 'github.com/iAmCorey/devtoolset', label: 'Devtoolset' },
-      gitStars: 203,
-      gitForks: 67
+      name: 'disckit',
+      description: 'An open source utilities library for the disutils bots',
+      link: { href: 'github.com/disutils/disckit', label: 'disckit' },
+      gitStars: 1,
+      gitForks: 0
     },
     {
-      name: 'Corey Chiu Portfolio Template',
+      name: 'Is It Pink?',
       description:
         'portfolio template by corey chiu',
-      link: { href: 'github.com/iAmCorey/coreychiu-portfolio-template', label: 'Corey Chiu Portfolio Template' },
-      gitStars: 229,
-      gitForks: 30
+      link: { href: 'github.com/disutils/is-it.pink', label: 'Is It Pink?' },
+      gitStars: 0,
+      gitForks: 0
     },
     {
-      name: 'Chrome Extension Plasmo Template',
+      name: 'Discord.py-Template',
       description:
         'A chrome extension template using plasmo, tailwind css, shadcn/ui',
-      link: { href: 'github.com/iAmCorey/chrome-extension-plasmo-template', label: 'Chrome Extension Plasmo Template' },
-      gitStars: 54,
-      gitForks: 10
-    },
-    {
-      name: 'Awesome Indie Hacker Tools',
-      description:
-        '独立开发/出海开发相关技术栈及工具收录 / Find the best tools for indie hackers here',
-      link: { href: 'github.com/iAmCorey/awesome-indie-hacker-tools', label: 'Awesome Indie Hacker Tools' },
-      gitStars: 815,
-      gitForks: 69
-    },
-    {
-      name: 'Awesome AI Directory',
-      description:
-        'AI资源工具导航站收录 / Find all the best AI directories',
-      link: { href: 'github.com/iAmCorey/awesome-ai-directory', label: 'Awesome AI Directory' },
-      gitStars: 40,
-      gitForks: 7
-    },
-    {
-      name: 'Producthunt Daily Bot',
-      description:
-        'A bot getting product hunt daily top products',
-      link: { href: 'github.com/iAmCorey/producthunt-daily-bot', label: 'Producthunt Daily Bot' },
-      gitStars: 3,
-      gitForks: 3
-    },
-    {
-      name: 'Cantonese Echoes',
-      description:
-        'Cantonese Echoes / 粵語殘片',
-      link: { href: 'github.com/iAmCorey/Cantonese-Echoes', label: 'Cantonese Echoes' },
-      gitStars: 1
-    },
+      link: { href: 'github.com/RejectModders/Discord.py-Template', label: 'Discord.py-Template' },
+      gitStars: 1,
+      gitForks: 1
+    }
   ]
   
